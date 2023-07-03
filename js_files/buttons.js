733f0601e9ec0e675f6c7da18a91deae76b1b36a
PedroHPG <dodge7@tutanota.com>
2023-07-03 11:48:33 -0300

    Made buttons hide other sections when selected

--- js_files/buttons.js
+++ js_files/buttons.js
@@ -1,10 +1,15 @@
 
 function toggleVisibility(id){
     let section = document.getElementById(id);
-    if (section.style.display == "none"){
-        section.style.display = "block";
-    } else {
-        section.style.display = "none";
+    //Take all other sections you want to hide
+    let others = document.getElementById('picking-methods').getElementsByTagName('div');
+    
+    for (var i = 0; i < others.length; i++){
+        let current = others[i];
+        if (current == section){
+            current.style.display = "block";
+        } else {
+            current.style.display = "none";
+        }
     }
-
 }
