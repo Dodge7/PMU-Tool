c9692fbd872def2eebcee9df4b471db0b8e1f261
PedroHPG <dodge7@tutanota.com>
2023-07-03 11:34:38 -0300

    Added toggling visibility to picking modes

---
+++ js_files/buttons.js
@@ -0,0 +1,10 @@
+
+function toggleVisibility(id){
+    let section = document.getElementById(id);
+    if (section.style.display == "none"){
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+
+}
