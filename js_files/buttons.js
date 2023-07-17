a04e15aea3768d7219eeb12cbe949892bcedf985
PedroHPG <dodge7@tutanota.com>
2023-07-17 11:47:12 -0300

    Began implementing randomize functionality

--- js_files/buttons.js
+++ js_files/buttons.js
@@ -3,7 +3,7 @@ function toggleVisibility(id){
     let section = document.getElementById(id);
     //Take all other sections you want to hide
     let others = document.getElementById('picking-methods').getElementsByTagName('div');
-    
+    //Loop through all the sections, showing the selected one and hiding the rest
     for (var i = 0; i < others.length; i++){
         let current = others[i];
         if (current == section){
---
+++ js_files/random.js
@@ -0,0 +1,12 @@
+
+//add a Mode parameter to have different functionality for the different modes
+function randomize(){
+
+    let amount = parseInt(document.getElementById("random-amount").value);
+    //loop through units of the currently selected game, take all the Guaranteed ones, then fill up with Available ones
+    for(let i = amount; i > 0; i--){
+
+    }
+
+
+}
