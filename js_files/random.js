6d60a0ee1db6d4f1a991b219d59f3d59eb8367d8
PedroHPG <dodge7@tutanota.com>
2023-07-18 11:14:55 -0300

    Added function to select random units

--- js_files/random.js
+++ js_files/random.js
@@ -8,13 +8,32 @@ function randomizeUnits(){
         'Revelations': 'revelationsUnits',
     }
     let amount = parseInt(document.getElementById("random-amount").value);
-    let currentGame = sessionStorage.getItem('game');
-    let currentUnitList = gameList[currentGame];
+    let currentUnitList = gameList[sessionStorage.getItem('game')];
+    let selectedList = [];
 
-    //loop through units of the currently selected game, take all the Guaranteed ones, then fill up with Available ones
-    for(let i = amount; i > 0; i--){
-
+    //loop through the unit list and grab all the guaranteed ones, removing all the banned ones
+    for(unit in currentUnitList){
+        if(unit["Availability"] == "Guaranteed"){
+            selectedList.push(unit);
+            currentUnitList.splice(indexOf(unit),1);
+        }
+        else if(unit["Availability"] == "Banned"){
+            currentUnitList.splice(indexOf(unit),1);
+        }
     }
 
+    //loop through units of the currently selected game and fill up with Available ones
+    for(let i = amount - selectedList.length; i > 0; i--){
+        
+        //pick a random unit
+        var randomUnit = function (currentUnitList) {
+            var keys = Object.keys(currentUnitList);
+            return currentUnitList[keys[ keys.length * Math.random() << 0]];
+        };
+        //add them to our list and remove from the array
+        selectedList.push(randomUnit);
+        currentUnitList.splice(indexOf(randomUnit),1);
+    }
 
+    return currentUnitList;
 }
